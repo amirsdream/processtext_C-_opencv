fa71d673959c4de21d6e9b13c75fc9ea4255b46a
amirsdream <amirsdream@gmail.com>
2018-09-20 11:54:38 +0300

    fix c++ raspberry

---
+++ cmake-build-debug/CMakeFiles/processtext_C-_opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processtext_C-_opencv.dir/main.cpp.o"
+  "processtext_C-_opencv.pdb"
+  "processtext_C-_opencv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/processtext_C-_opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
